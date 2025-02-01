73dd5b90255a95d61fed657788e7951b9cd86ab3
Channa Dissanayaka <ywchanna@gmail.com>
2025-02-01 09:52:26 +0530

    modified

--- team_assigner/team_assigner.py
+++ team_assigner/team_assigner.py
@@ -2,7 +2,7 @@ from sklearn.cluster import KMeans
 
 class TeamAssigner:
     def __init__(self):
-        pass
+        self.team_colors = {}
 
     def get_clustering_model(self, image):
         #Reshape the image to 2D array
@@ -27,13 +27,28 @@ class TeamAssigner:
         # Reshape the labels to the image shape
         clustered_image = labels.reshape(top_half_image.shape[0], top_half_image.shape[1])
 
+        # Get the player cluster
+        corner_clusters = [clustered_image[0,0], clustered_image[0,-1],clustered_image[-1,0],clustered_image[-1,-1]]
+        non_player_cluster = max(set(corner_clusters),key=corner_clusters.count)
+        player_cluster = 1 - non_player_cluster
 
+        player_color = kmeans.cluster_centers_[player_cluster]
+        return player_color
 
 
 
 
     def assign_team_color(self, frame, player_detections):
-        player_color = []
+        player_colors = []
         for _, player_detection in player_detections.items():
             bbox = player_detection["bbox"]
             player_color = self.get_player_color(frame, bbox)
+            player_colors.append(player_color)
+        
+
+        kmeans = kmeans(n_clusters=2, init="k-means++", n_init=1)
+        kmeans.fit(player_colors)
+
+        self.team_colors[1] = kmeans.cluster_centers_[0]
+        self.team_colors[2] = kmeans.cluster_centers_[1]
+
