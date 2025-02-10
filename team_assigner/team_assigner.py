039b5580f7b029207e294dda561c360842d7e861
Channa Dissanayaka <ywchanna@gmail.com>
2025-02-10 21:54:30 +0530

    seperate players accoding to thier teams

--- team_assigner/team_assigner.py
+++ team_assigner/team_assigner.py
@@ -3,6 +3,7 @@ from sklearn.cluster import KMeans
 class TeamAssigner:
     def __init__(self):
         self.team_colors = {}
+        self.player_team_dict = {}
 
     def get_clustering_model(self, image):
         #Reshape the image to 2D array
@@ -46,9 +47,25 @@ class TeamAssigner:
             player_colors.append(player_color)
         
 
-        kmeans = kmeans(n_clusters=2, init="k-means++", n_init=1)
+        kmeans = KMeans(n_clusters=2, init="k-means++", n_init=1)
         kmeans.fit(player_colors)
+
+        self.kmeans = kmeans
 
         self.team_colors[1] = kmeans.cluster_centers_[0]
         self.team_colors[2] = kmeans.cluster_centers_[1]
+
+    def get_player_team(self,frame,player_bbox,player_id):
+        if player_id in self.player_team_dict:
+            return self.player_team_dict[player_id]
+        
+        player_color = self.get_player_color(frame,player_bbox)
+
+        team_id = self.kmeans.predict(player_color.reshape(1,-1))[0]
+        team_id+=1
+
+        self.player_team_dict[player_id] = team_id
+
+        return team_id
+
 
