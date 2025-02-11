7e3f02408798888cbda6cf828b92bed7a27b70d6
Channa Dissanayaka <ywchanna@gmail.com>
2025-02-11 16:24:39 +0530

    nearest player tracking

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -171,6 +171,9 @@ class Tracker:
                 color = player.get("team_color",(0,0,255))
                 frame = self.draw_ellipse(frame, player["bbox"], color, track_id)
 
+                if player.get('has_ball',False):
+                    frame = self.draw_triangle(frame, player["bbox"],(255,0,0))
+
             #Draw Referee
             for _, referee in referee_dict.items():
                 frame = self.draw_ellipse(frame, referee["bbox"], (0,255,255))
